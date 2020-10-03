6ec8f360695fa3332b681e4339f57db1ebc93e72
Dave Marples <dave@marples.net>
2020-10-03 22:28:47 +0100

    Fix lack of typecast warning

--- Src/orbcat.c
+++ Src/orbcat.c
@@ -229,17 +229,18 @@ void _handleSW( struct ITMDecoder *i )
         {
             /* Build 32 value the long way around to avoid type-punning issues */
             w = ( p.d[3] << 24 ) | ( p.d[2] << 16 ) | ( p.d[1] << 8 ) | ( p.d[0] );
-            if ( strstr(options.presFormat[p.srcAddr], "%f") > 0 )
+
+            if ( strstr( options.presFormat[p.srcAddr], "%f" ) > 0 )
             {
                 /* type punning on same host, after correctly building 32bit val
                  * only unsafe on systems where u32/float have diff byte order */
-                float *nastycast = &w;
+                float *nastycast = ( float * )&w;
                 fprintf( stdout, options.presFormat[p.srcAddr], *nastycast );
             }
             else
             {
                 fprintf( stdout, options.presFormat[p.srcAddr], w );
-	    }
+            }
         }
     }
 }
