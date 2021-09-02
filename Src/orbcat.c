1e4b77ea3cd38988315fd287159dd5fc151f35b6
Karl Palsson <karlp@etactica.com>
2021-09-02 11:39:06 +0000

    orbcat: reconnect sockets on failure
    If you have to restart openocd (for instance, because you've changed
    track clock speeds) you currently need to re-start orbcat as well.
    
    Make it reconnect by default, and use the existing "e" option for
    ~similar behaviour as with files.  "wait for more" unless explicitly
    requested to exit.
    
    Signed-off-by: Karl Palsson <karlp@tweak.net.au>

--- Src/orbcat.c
+++ Src/orbcat.c
@@ -93,7 +93,7 @@ struct
     char *server;
 
     char *file;                                          /* File host connection */
-    bool fileTerminate;                                  /* Terminate when file read isn't successful */
+    bool endTerminate;                                  /* Terminate when file/socket "ends" */
 } options = {.forceITMSync = true, .tpiuITMChannel = 1, .port = SERVER_PORT, .server = "localhost"};
 
 struct
@@ -380,7 +380,7 @@ void _printHelp( char *progName )
 {
     fprintf( stdout, "Usage: %s <htv> <-i channel> <-p port> <-s server>" EOL, progName );
     fprintf( stdout, "       c: <Number>,<Format> of channel to add into output stream (repeat per channel)" EOL );
-    fprintf( stdout, "       e: When reading from file, terminate at end of file rather than waiting for further input" EOL );
+    fprintf( stdout, "       e: Terminate when the file/socket ends/is closed, or attempt to wait for more / reconnect" EOL );
     fprintf( stdout, "       f: <filename> Take input from specified file" EOL );
     fprintf( stdout, "       h: This help" EOL );
     fprintf( stdout, "       i: <channel> Set ITM Channel in TPIU decode (defaults to 1)" EOL );
@@ -409,7 +409,7 @@ int _processOptions( int argc, char *argv[] )
 
             // ------------------------------------
             case 'e':
-                options.fileTerminate = true;
+                options.endTerminate = true;
                 break;
 
             // ------------------------------------
@@ -525,7 +525,7 @@ int _processOptions( int argc, char *argv[] )
 
         genericsReport( V_INFO, "Input File : %s", options.file );
 
-        if ( options.fileTerminate )
+        if ( options.endTerminate )
         {
             genericsReport( V_INFO, " (Terminate on exhaustion)" EOL );
         }
@@ -575,7 +575,7 @@ int fileFeeder( void )
 
         if ( !t )
         {
-            if ( options.fileTerminate )
+            if ( options.endTerminate )
             {
                 break;
             }
@@ -595,7 +595,7 @@ int fileFeeder( void )
         }
     }
 
-    if ( !options.fileTerminate )
+    if ( !options.endTerminate )
     {
         genericsReport( V_INFO, "File read error" EOL );
     }
@@ -605,7 +605,7 @@ int fileFeeder( void )
 }
 
 // ====================================================================================================
-int main( int argc, char *argv[] )
+int socketFeeder( void )
 
 {
     int sockfd;
@@ -615,15 +615,6 @@ int main( int argc, char *argv[] )
     ssize_t t;
     int flag = 1;
 
-    if ( !_processOptions( argc, argv ) )
-    {
-        exit( -1 );
-    }
-
-    /* Reset the TPIU handler before we start */
-    TPIUDecoderInit( &_r.t );
-    ITMDecoderInit( &_r.i, options.forceITMSync );
-
     sockfd = socket( AF_INET, SOCK_STREAM, 0 );
     setsockopt( sockfd, SOL_SOCKET, SO_REUSEPORT, &flag, sizeof( flag ) );
 
@@ -631,11 +622,6 @@ int main( int argc, char *argv[] )
     {
         genericsReport( V_ERROR, "Error creating socket" EOL );
         return -1;
-    }
-
-    if ( options.file )
-    {
-        exit( fileFeeder() );
     }
 
     /* Now open the network connection */
@@ -676,5 +662,33 @@ int main( int argc, char *argv[] )
 
     close( sockfd );
     return -2;
+
+}
+
+// ====================================================================================================
+int main( int argc, char *argv[] )
+
+{
+    if ( !_processOptions( argc, argv ) )
+    {
+        exit( -1 );
+    }
+
+    /* Reset the TPIU handler before we start */
+    TPIUDecoderInit( &_r.t );
+    ITMDecoderInit( &_r.i, options.forceITMSync );
+
+    if ( options.file )
+    {
+        exit( fileFeeder() );
+    }
+
+    do {
+	    int rc = socketFeeder();
+	    // TODO - make logging of failures/reconnections "nicer" based on rc?
+	    (void)rc;
+	    // tradeoff to re-attach "promptly" vs CPU spinning and log spam
+	    usleep(100*1000);
+    } while (!options.endTerminate);
 }
 // ====================================================================================================
